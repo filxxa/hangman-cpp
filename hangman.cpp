d4d69d78470cf1742fddb30e37f756f7b61d53d5
filxxa <filzaj13@gmail.com>
2026-01-03 09:41:16 -0800

    Initial commit

---
+++ hangman.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"===HANGMAN==="<<endl;
+    string word = "example";
+    string guessed(word.length(), '_');
+    int attempts = 6;
+    char guess;
+    cout<<"Guess the word: " << guessed << endl;
+    while (attempts > 0 && guessed != word) {
+        cout << "Enter a letter: ";
+        cin >> guess;
+        bool correct = false;
+        for (int i = 0; i < word.length(); i++) {
+            if (word[i] == guess) {
+                guessed[i] = guess;
+                correct = true;
+            }
+        }
+        if (correct) {
+            cout<<"Correct!" << endl;
+        } else {
+            attempts--;
+            cout << "Wrong guess! Attempts left: " << attempts << endl;
+        }
+        cout << "Current word: " << guessed << endl;
+    }
+    return 0;
+}
